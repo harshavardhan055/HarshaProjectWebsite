cccc6c10411b506a6ce42b9677d3b48ed449c0a6
harshavardhan055 <mastertechnical193@gmail.com>
2025-06-05 05:01:40 +0530

    Update routes.py

--- routes.py
+++ routes.py
@@ -276,5 +276,20 @@ def profile():
 def contact():
     return render_template("contact.html")
 
+# ---------------------- Error Handlers ----------------------
+
+@main_routes.app_errorhandler(403)
+def forbidden_error(error):
+    return render_template("403.html"), 403
+
+@main_routes.app_errorhandler(404)
+def not_found_error(error):
+    return render_template("404.html"), 404
+
+@main_routes.app_errorhandler(500)
+def internal_error(error):
+    return render_template("500.html"), 500
+
+
 
 
