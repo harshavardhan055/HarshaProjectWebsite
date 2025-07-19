0266d6518b56a019923aec55f05fe014929be9eb
harshavardhan055 <mastertechnical193@gmail.com>
2025-07-20 00:35:33 +0530

    Update app_init.py

--- app_init.py
+++ app_init.py
@@ -1,9 +1,8 @@
-# app_init.py
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///harsha_projects.db'
-app.config['SECRET_KEY'] = 'your-secret-key'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///harsha_projects.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
