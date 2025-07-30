dd6d30774267fbc14c575fa2bf6e84c7165dbd81
harshavardhan055 <mastertechnical193@gmail.com>
2025-07-31 00:44:14 +0530

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,9 +1,6 @@
+# extensions.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from sqlalchemy.orm import DeclarativeBase
 
-class Base(DeclarativeBase):
-    pass
-
-db = SQLAlchemy(model_class=Base)
+db = SQLAlchemy()
 login_manager = LoginManager()
